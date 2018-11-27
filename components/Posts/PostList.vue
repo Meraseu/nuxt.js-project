453a416f41437d15289f78204d288e49348666fe
meraseudev <meraseudev@gmail.com>
2018-11-27 13:10:37 +0900

    gitignore 수정

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="post-list">
-        <PostPreview id="1" thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg" title="Hello there!" previewText="This my first post!" />
-        <PostPreview id="2" thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg" title="Hello there!" previewText="This my second post!" />
-        <PostPreview id="3" thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg" title="Hello there!" previewText="This my third post!" />
+        <PostPreview id="1" :is-admin="isAdmin" thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg" title="Hello there!" previewText="This my first post!" />
+        <PostPreview id="2" :is-admin="isAdmin" thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg" title="Hello there!" previewText="This my second post!" />
+        <PostPreview id="3" :is-admin="isAdmin" thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg" title="Hello there!" previewText="This my third post!" />
     </section>
 </template>
 
@@ -12,6 +12,12 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
   components: {
     PostPreview: PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 }
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+export default {
+    layout: 'admin',
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Meraseudev',
+                title: 'My Awesome Post',
+                content: 'Content',
+                thumbnailLink: 'https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg'
+            }
+        }
+    }
+}
+</script>
